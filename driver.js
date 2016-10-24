4c731334b31d381c37d76d2f58e27bd671c2cabb
Alex Kwan <alexkwan089@gmail.com>
2016-10-24 08:34:56 -0700

    jslint-ified the files, there are known issues with driver.js due to the test cases

--- driver.js
+++ driver.js
@@ -1,28 +1,59 @@
+"use strict";
+
 var parser = require('./index.js');
 var nullLink = null;
 var undefinedLink = null;
-var noWdLink = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote'
-var urlWithBracketsEdgeCase = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBlah%28%5C%29%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29'
-var urlWithLinesEdgeCase = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBla%5C%7C%5C%7C%5C%7Ch%28%5C%29%5C%7C%5C%7C%5C%7C%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29'
-var urlWithMultipleForwardSlashes = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBl%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F%5C%2Fa%5C%7C%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29'
-var moreMultipleForwardSlashesAndBracketsUrl = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBl%5C%2F%5C%2F%5C%5C%5C%5C%5C%5C%5C%5C%5C%5C%5C%2F%5C%2F%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29'
-var goodLink = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2FTypescript%20Definition%20Files.one%7C865d0e84-d32b-44c4-8b2c-e746e460ce8a%2FExample%20Javascript%20Library%20%28OneNote%20Deep%20Link%5C%29%7Ceea62e68-1529-4eec-83cf-08c4498c4b96%2F%29'
-var dotoneinsectionnameurl = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ft.one.two.three.one%7C3cd6ec7a-e49b-4bc1-bfb3-57af7865a046%2F.one.two.two%7C68ec67a4-2345-433d-9150-1bd51d80b463%2F%29'
-var validsharepointurl = 'https://msft.spoppe.com/teams/onenote/_layouts/15/WopiFrame.aspx?sourcedoc={ea3d1219-b013-4a19-b301-bfdea52eb922}&action=edit&wd=target%28%2F%2FOCE%2FData%20and%20Examples.one%7Cf5107145-d77a-481e-ac3d-17e60ea89af7%2FKusto%20Queries%7C5a485da3-1c7c-4833-ab29-f77ab60dcea2%2F%29'
+var noWdLink = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote';
+var urlWithBracketsEdgeCase = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBlah%28%5C%29%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29';
+var urlWithLinesEdgeCase = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBla%5C%7C%5C%7C%5C%7Ch%28%5C%29%5C%7C%5C%7C%5C%7C%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29';
+var urlWithMultipleForwardSlashes = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBl%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F%5C%2Fa%5C%7C%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29';
+var moreMultipleForwardSlashesAndBracketsUrl = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ftest%5C%29%5C%29.one%7Cb61b035e-278a-4796-ab82-42e1bda4d9c7%2FBl%5C%2F%5C%2F%5C%5C%5C%5C%5C%5C%5C%5C%5C%5C%5C%2F%5C%2F%7C13d3a0df-79de-4ad3-886d-9dc62b1be675%2F%29';
+var goodLink = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2FTypescript%20Definition%20Files.one%7C865d0e84-d32b-44c4-8b2c-e746e460ce8a%2FExample%20Javascript%20Library%20%28OneNote%20Deep%20Link%5C%29%7Ceea62e68-1529-4eec-83cf-08c4498c4b96%2F%29';
+var dotoneinsectionnameurl = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2Ft.one.two.three.one%7C3cd6ec7a-e49b-4bc1-bfb3-57af7865a046%2F.one.two.two%7C68ec67a4-2345-433d-9150-1bd51d80b463%2F%29';
+var validsharepointurl = 'https://msft.spoppe.com/teams/onenote/_layouts/15/WopiFrame.aspx?sourcedoc={ea3d1219-b013-4a19-b301-bfdea52eb922}&action=edit&wd=target%28%2F%2FOCE%2FData%20and%20Examples.one%7Cf5107145-d77a-481e-ac3d-17e60ea89af7%2FKusto%20Queries%7C5a485da3-1c7c-4833-ab29-f77ab60dcea2%2F%29';
+
+function verify(testcase, url, expectedOutcome) {
+    var parsed, sectionNameMatches, sectionIdMatches, pageNameMatches, pageIdMatches;
+
+    console.log('Test Case : ' + testcase);
+    parsed = parser(url);
+
+    sectionNameMatches = expectedOutcome.sectionName === parsed.sectionName;
+    if (!sectionNameMatches) {
+        console.log('ExpectedSectionName : ' + expectedOutcome.sectionName + ' !==  ActualSectionName : ' + parsed.sectionName);
+    }
+
+    sectionIdMatches = expectedOutcome.sectionId === parsed.sectionId;
+    if (!sectionIdMatches) {
+        console.log('ExpectedSectionId : ' + expectedOutcome.sectionId + ' !== ActualSectionId : ' + parsed.sectionId);
+    }
+
+    pageNameMatches = expectedOutcome.pageName === parsed.pageName;
+    if (!pageNameMatches) {
+        console.log('ExpectedPageName : ' + expectedOutcome.pageName + ' !== ActualPageName : ' + parsed.pageName);
+    }
+
+    pageIdMatches = expectedOutcome.pageId === parsed.pageId;
+    if (!pageIdMatches) {
+        console.log('ExpectedPageId : ' + expectedOutcome.pageId + ' !== ActualPageId : ' + parsed.pageId);
+    }
+
+    console.log(sectionNameMatches && sectionIdMatches && pageNameMatches && pageIdMatches);
+}
 
 verify('nullLink', nullLink, {
     sectionName : null,
     sectionId : null,
     pageName : null,
     pageId : null
-})
+});
 
 verify('undefinedLink', undefinedLink, {
     sectionName : null,
     sectionId : null,
     pageName : null,
     pageId : null
-})
+});
 
 verify('noWdLink', noWdLink, {
     sectionName : null,
@@ -36,74 +67,46 @@ verify('urlWithBracketsEdgeCase', urlWithBracketsEdgeCase, {
     sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
     pageName : 'Blah(\)',
     pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
-})
+});
 
 verify('urlWithLinesEdgeCase', urlWithLinesEdgeCase, {
     sectionName : 'test\)\)',
     sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
     pageName : 'Bla\|\|\|h(\)\|\|\|',
     pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
-} )
+});
 
 verify('urlWithMultipleForwardSlashes', urlWithMultipleForwardSlashes, {
     sectionName : 'test\)\)',
     sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
     pageName : 'Bl///////a|',
     pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
-})
+});
 
 verify('moreMultipleForwardSlashesAndBracketsUrl', moreMultipleForwardSlashesAndBracketsUrl, {
     sectionName : 'test))',
     sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
     pageName : 'Bl////',
     pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
-})
+});
 
 verify('goodLink', goodLink, {
     sectionName : 'Typescript Definition Files',
     sectionId : '865d0e84-d32b-44c4-8b2c-e746e460ce8a',
     pageName : 'Example Javascript Library (OneNote Deep Link)',
     pageId : 'eea62e68-1529-4eec-83cf-08c4498c4b96'
-})
+});
 
 verify('dotoneinsectionnameurl', dotoneinsectionnameurl, {
     sectionName : 't.one.two.three',
     sectionId : '3cd6ec7a-e49b-4bc1-bfb3-57af7865a046',
     pageName : '.one.two.two',
     pageId : '68ec67a4-2345-433d-9150-1bd51d80b463'
-})
+});
 
 verify('validsharepointurl', validsharepointurl, {
     sectionName : 'OCE/Data and Examples',
     sectionId : 'f5107145-d77a-481e-ac3d-17e60ea89af7',
     pageName : 'Kusto Queries',
     pageId : '5a485da3-1c7c-4833-ab29-f77ab60dcea2'
-})
-
-function verify(testcase, url, expectedOutcome){
-    console.log('Test Case : '+testcase);
-    var parsed = parser(url);
-
-    var sectionNameMatches = expectedOutcome.sectionName === parsed.sectionName;
-    if(!sectionNameMatches){
-        console.log('ExpectedSectionName : '+ expectedOutcome.sectionName +' !==  ActualSectionName : ' +parsed.sectionName);
-    }
-
-    var sectionIdMatches = expectedOutcome.sectionId === parsed.sectionId;
-    if(!sectionIdMatches){
-        console.log('ExpectedSectionId : '+ expectedOutcome.sectionId +' !== ActualSectionId : ' +parsed.sectionId);
-    }
-
-    var pageNameMatches = expectedOutcome.pageName === parsed.pageName;
-    if(!pageNameMatches){
-        console.log('ExpectedPageName : '+ expectedOutcome.pageName +' !== ActualPageName : ' +parsed.pageName);
-    }
-
-    var pageIdMatches = expectedOutcome.pageId === parsed.pageId;
-    if(!pageIdMatches){
-        console.log('ExpectedPageId : '+ expectedOutcome.pageId +' !== ActualPageId : ' +parsed.pageId);
-    }
-
-    console.log(sectionNameMatches && sectionIdMatches && pageNameMatches && pageIdMatches);
-
-}
+});
--- index.js
+++ index.js
@@ -1,45 +1,91 @@
-function parse(url){
+"use strict";
+
+// retrieves query parameters by name
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function isTarget(queryParameter) {
+    return queryParameter.startsWith('target(') && queryParameter.endsWith(')');
+}
+
+function retrieveTargetInformation(queryParameter) {
+    // format of target query parameter   target(<targetinfo>)
+    return queryParameter.substring(7, queryParameter.length - 1);
+}
+
+//http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+function parse(url) {
+    var pageDelimiterLength = 1,
+        guidLength = 36,
+        wd,
+        targetInfo,
+        sectionDelimiterPosition,
+        afterSectionDelimiter,
+        targetInfoLength,
+        sectionGuid,
+        sectionName,
+        pageName,
+        pageId;
+
+
+
     // null or undefined case
-   if (typeof(url) == "undefined" || url == null){
-       return {
-           isValidUrl : false,
-           sectionName : null,
-           sectionId : null,
-           pageName : null,
-           pageId : null,
-           url : url,
-           reason : 'url was null or undefined'
-       }
-   }
+    if (url === "undefined" || url === null) {
+        return {
+            isValidUrl : false,
+            sectionName : null,
+            sectionId : null,
+            pageName : null,
+            pageId : null,
+            url : url,
+            reason : 'url was null or undefined'
+        };
+    }
 
-   var wd = getParameterByName("wd", url);
+    wd = getParameterByName("wd", url);
 
-   if(typeof(wd) == "undefined" || wd == null || !isTarget(wd)){
-      return {
-           isValidUrl : false,
-           sectionName : null,
-           sectionId : null,
-           pageName : null,
-           pageId : null,
-           url : url,
-           reason : 'there was no wd parameter or it did not contain a target'
-       } 
-   }
+    if (wd === "undefined" || wd === null || !isTarget(wd)) {
+        return {
+            isValidUrl : false,
+            sectionName : null,
+            sectionId : null,
+            pageName : null,
+            pageId : null,
+            url : url,
+            reason : 'there was no wd parameter or it did not contain a target'
+        };
+    }
 
-   var targetInfo = retrieveTargetInformation(wd)
-    
+    targetInfo = retrieveTargetInformation(wd);
+
     // Section names can't contain any of the following characters: ~ # % & ( { } | \ : " < > ? / ^
     try {
-        var sectionDelimiterPosition = targetInfo.indexOf("|");
-        var afterSectionDelimiter = sectionDelimiterPosition + 1;
-        var pageDelimiterLength = 1;
-        var guidLength = 36
-        var targetInfoLength = targetInfo.length - 1
+        sectionDelimiterPosition = targetInfo.indexOf("|");
+        afterSectionDelimiter = sectionDelimiterPosition + 1;
+        targetInfoLength = targetInfo.length - 1;
 
-        var sectionGuid = targetInfo.substring(afterSectionDelimiter , afterSectionDelimiter + guidLength)
-        var sectionName = targetInfo.substring(2 /* \\ */, sectionDelimiterPosition - 4 /* .one */)
-        var pageName = targetInfo.substring(afterSectionDelimiter + guidLength + pageDelimiterLength, targetInfoLength-guidLength - pageDelimiterLength);
-        var pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength)
+        sectionGuid = targetInfo.substring(afterSectionDelimiter, afterSectionDelimiter + guidLength);
+
+        // format for section name in target string \\<sectionname>.one
+        sectionName = targetInfo.substring(2, sectionDelimiterPosition - 4);
+        pageName = targetInfo.substring(afterSectionDelimiter + guidLength + pageDelimiterLength, targetInfoLength - guidLength - pageDelimiterLength);
+        pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength);
 
         return {
             isValidUrl : true,
@@ -49,47 +95,18 @@ function parse(url){
             pageId : pageId,
             url : url,
             reason : 'success'
-        }
-    }
-    catch(e){
+        };
+    } catch (e) {
         return {
-           isValidUrl : false,
-           sectionName : null,
-           sectionId : null,
-           pageName : null,
-           pageId : null,
-           url : url,
-           reason : 'unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )'
-        }
+            isValidUrl : false,
+            sectionName : null,
+            sectionId : null,
+            pageName : null,
+            pageId : null,
+            url : url,
+            reason : 'unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )'
+        };
     }
 }
-
-// retrieves query parameters by name
-// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-function getParameterByName(name, url){
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function isTarget(queryParameter){
-    return queryParameter.startsWith('target(') && queryParameter.endsWith(')')
-}
-
-function retrieveTargetInformation(queryParameter){
-    return queryParameter.substring(7 /* length of "target(" */, queryParameter.length-1 /* cut off the trailing ")" */)
-}
-
-//http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};
 
 module.exports = parse;
