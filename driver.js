75eba937204bb32b2a4c24a92de530927188ffdd
Alex Kwan <alexkwan089@gmail.com>
2016-10-24 22:40:45 -0700

    jscs style across the project

--- driver.js
+++ driver.js
@@ -13,107 +13,132 @@ var dotoneinsectionnameurl = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab7
 var validsharepointurl = 'https://msft.spoppe.com/teams/onenote/_layouts/15/WopiFrame.aspx?sourcedoc={ea3d1219-b013-4a19-b301-bfdea52eb922}&action=edit&wd=target%28%2F%2FOCE%2FData%20and%20Examples.one%7Cf5107145-d77a-481e-ac3d-17e60ea89af7%2FKusto%20Queries%7C5a485da3-1c7c-4833-ab29-f77ab60dcea2%2F%29';
 var anotherValidOneDriveLink = 'https://onedrive.live.com/edit.aspx?cid=7d4ca48ab71fa43a&id=documents&resid=7D4CA48AB71FA43A!1002&app=OneNote&&wd=target%28%2F%2FTypescript%20Definition%20Files.one%7C865d0e84-d32b-44c4-8b2c-e746e460ce8a%2FRoadmap%7Cb32b75bb-693a-4fce-872e-19b7681f7e0c%2F%29';
 function verify(testcase, url, expectedOutcome) {
-    var parsed, sectionNameMatches, sectionIdMatches, pageNameMatches, pageIdMatches;
+  var parsed, sectionNameMatches, sectionIdMatches, pageNameMatches, pageIdMatches;
 
-    console.log('Test Case : ' + testcase);
-    parsed = parser(url);
+  console.log('Test Case : ' + testcase);
+  parsed = parser(url);
 
-    sectionNameMatches = expectedOutcome.sectionName === parsed.sectionName;
-    if (!sectionNameMatches) {
-        console.log('ExpectedSectionName : ' + expectedOutcome.sectionName + ' !==  ActualSectionName : ' + parsed.sectionName);
-    }
+  sectionNameMatches = expectedOutcome.sectionName === parsed.sectionName;
+  if (!sectionNameMatches) {
+    console.log(
+      'ExpectedSectionName : ' +
+      expectedOutcome.sectionName +
+      ' !==  ActualSectionName : ' +
+      parsed.sectionName
+    );
+  }
 
-    sectionIdMatches = expectedOutcome.sectionId === parsed.sectionId;
-    if (!sectionIdMatches) {
-        console.log('ExpectedSectionId : ' + expectedOutcome.sectionId + ' !== ActualSectionId : ' + parsed.sectionId);
-    }
+  sectionIdMatches = expectedOutcome.sectionId === parsed.sectionId;
+  if (!sectionIdMatches) {
+    console.log(
+      'ExpectedSectionId : ' +
+      expectedOutcome.sectionId +
+      ' !== ActualSectionId : ' +
+      parsed.sectionId
+    );
+  }
 
-    pageNameMatches = expectedOutcome.pageName === parsed.pageName;
-    if (!pageNameMatches) {
-        console.log('ExpectedPageName : ' + expectedOutcome.pageName + ' !== ActualPageName : ' + parsed.pageName);
-    }
+  pageNameMatches = expectedOutcome.pageName === parsed.pageName;
+  if (!pageNameMatches) {
+    console.log(
+      'ExpectedPageName : ' +
+      expectedOutcome.pageName +
+      ' !== ActualPageName : ' +
+      parsed.pageName
+    );
+  }
 
-    pageIdMatches = expectedOutcome.pageId === parsed.pageId;
-    if (!pageIdMatches) {
-        console.log('ExpectedPageId : ' + expectedOutcome.pageId + ' !== ActualPageId : ' + parsed.pageId);
-    }
+  pageIdMatches = expectedOutcome.pageId === parsed.pageId;
+  if (!pageIdMatches) {
+    console.log(
+      'ExpectedPageId : ' +
+      expectedOutcome.pageId +
+      ' !== ActualPageId : ' +
+      parsed.pageId
+    );
+  }
 
-    console.log(sectionNameMatches && sectionIdMatches && pageNameMatches && pageIdMatches);
+  console.log(
+    sectionNameMatches &&
+    sectionIdMatches &&
+    pageNameMatches &&
+    pageIdMatches)
+  ;
 }
 
 verify('nullLink', nullLink, {
-    sectionName : null,
-    sectionId : null,
-    pageName : null,
-    pageId : null
+  sectionName: null,
+  sectionId: null,
+  pageName: null,
+  pageId: null,
 });
 
 verify('undefinedLink', undefinedLink, {
-    sectionName : null,
-    sectionId : null,
-    pageName : null,
-    pageId : null
+  sectionName: null,
+  sectionId: null,
+  pageName: null,
+  pageId: null,
 });
 
 verify('noWdLink', noWdLink, {
-    sectionName : null,
-    sectionId : null,
-    pageName : null,
-    pageId : null
+  sectionName: null,
+  sectionId: null,
+  pageName: null,
+  pageId: null,
 });
 
 verify('urlWithBracketsEdgeCase', urlWithBracketsEdgeCase, {
-    sectionName : 'test\)\)',
-    sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
-    pageName : 'Blah(\)',
-    pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
+  sectionName: 'test\)\)',
+  sectionId: 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
+  pageName: 'Blah(\)',
+  pageId: '13d3a0df-79de-4ad3-886d-9dc62b1be675',
 });
 
 verify('urlWithLinesEdgeCase', urlWithLinesEdgeCase, {
-    sectionName : 'test\)\)',
-    sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
-    pageName : 'Bla\|\|\|h(\)\|\|\|',
-    pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
+  sectionName: 'test\)\)',
+  sectionId: 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
+  pageName: 'Bla\|\|\|h(\)\|\|\|',
+  pageId: '13d3a0df-79de-4ad3-886d-9dc62b1be675',
 });
 
 verify('urlWithMultipleForwardSlashes', urlWithMultipleForwardSlashes, {
-    sectionName : 'test\)\)',
-    sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
-    pageName : 'Bl///////a|',
-    pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
+  sectionName: 'test\)\)',
+  sectionId: 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
+  pageName: 'Bl///////a|',
+  pageId: '13d3a0df-79de-4ad3-886d-9dc62b1be675',
 });
 
 verify('moreMultipleForwardSlashesAndBracketsUrl', moreMultipleForwardSlashesAndBracketsUrl, {
-    sectionName : 'test))',
-    sectionId : 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
-    pageName : 'Bl////',
-    pageId : '13d3a0df-79de-4ad3-886d-9dc62b1be675'
+  sectionName: 'test))',
+  sectionId: 'b61b035e-278a-4796-ab82-42e1bda4d9c7',
+  pageName: 'Bl////',
+  pageId: '13d3a0df-79de-4ad3-886d-9dc62b1be675',
 });
 
 verify('goodLink', goodLink, {
-    sectionName : 'Typescript Definition Files',
-    sectionId : '865d0e84-d32b-44c4-8b2c-e746e460ce8a',
-    pageName : 'Example Javascript Library (OneNote Deep Link)',
-    pageId : 'eea62e68-1529-4eec-83cf-08c4498c4b96'
+  sectionName: 'Typescript Definition Files',
+  sectionId: '865d0e84-d32b-44c4-8b2c-e746e460ce8a',
+  pageName: 'Example Javascript Library (OneNote Deep Link)',
+  pageId: 'eea62e68-1529-4eec-83cf-08c4498c4b96',
 });
 
 verify('dotoneinsectionnameurl', dotoneinsectionnameurl, {
-    sectionName : 't.one.two.three',
-    sectionId : '3cd6ec7a-e49b-4bc1-bfb3-57af7865a046',
-    pageName : '.one.two.two',
-    pageId : '68ec67a4-2345-433d-9150-1bd51d80b463'
+  sectionName: 't.one.two.three',
+  sectionId: '3cd6ec7a-e49b-4bc1-bfb3-57af7865a046',
+  pageName: '.one.two.two',
+  pageId: '68ec67a4-2345-433d-9150-1bd51d80b463',
 });
 
 verify('validsharepointurl', validsharepointurl, {
-    sectionName : 'Data and Examples',
-    sectionId : 'f5107145-d77a-481e-ac3d-17e60ea89af7',
-    pageName : 'Kusto Queries',
-    pageId : '5a485da3-1c7c-4833-ab29-f77ab60dcea2'
+  sectionName: 'Data and Examples',
+  sectionId: 'f5107145-d77a-481e-ac3d-17e60ea89af7',
+  pageName: 'Kusto Queries',
+  pageId: '5a485da3-1c7c-4833-ab29-f77ab60dcea2',
 });
 
 verify('anotherValidOneDriveLink', anotherValidOneDriveLink, {
-    sectionName : 'Typescript Definition Files',
-    sectionId : '865d0e84-d32b-44c4-8b2c-e746e460ce8a',
-    pageName : 'Roadmap',
-    pageId : 'b32b75bb-693a-4fce-872e-19b7681f7e0c'
+  sectionName: 'Typescript Definition Files',
+  sectionId: '865d0e84-d32b-44c4-8b2c-e746e460ce8a',
+  pageName: 'Roadmap',
+  pageId: 'b32b75bb-693a-4fce-872e-19b7681f7e0c',
 });
--- index.js
+++ index.js
@@ -1,121 +1,124 @@
-"use strict";
+'use strict';
 
 // retrieves query parameters by name
 // http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
 function getParameterByName(name, url) {
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) {
-        return null;
-    }
-    if (!results[2]) {
-        return '';
-    }
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  results = regex.exec(url);
+  if (!results) {
+    return null;
+  }
+
+  if (!results[2]) {
+    return '';
+  }
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 function isTarget(queryParameter) {
-    return queryParameter.startsWith('target(') && queryParameter.endsWith(')');
+  return queryParameter.startsWith('target(') && queryParameter.endsWith(')');
 }
 
 function retrieveTargetInformation(queryParameter) {
-    // format of target query parameter   target(<targetinfo>)
-    return queryParameter.substring(7, queryParameter.length - 1);
+  // format of target query parameter   target(<targetinfo>)
+  return queryParameter.substring(7, queryParameter.length - 1);
 }
 
 //http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
 String.prototype.replaceAll = function (search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
+  var target = this;
+  return target.split(search).join(replacement);
 };
 
 function parse(url) {
-    var pageDelimiterLength = 1,
-        guidLength = 36,
-        wd,
-        targetInfo,
-        sectionDelimiterPosition,
-        afterSectionDelimiter,
-        targetInfoLength,
-        sectionGuid,
-        sectionGroupNames,
-        sectionName,
-        pageName,
-        pageId;
+  var pageDelimiterLength = 1,
+    guidLength = 36,
+    wd,
+    targetInfo,
+    sectionDelimiterPosition,
+    afterSectionDelimiter,
+    targetInfoLength,
+    sectionGuid,
+    sectionGroupNames,
+    sectionName,
+    pageName,
+    pageId;
 
+  // null or undefined case
+  if (url === 'undefined' || url === null) {
+    return {
+      isValidUrl: false,
+      sectionGroupNames: [],
+      sectionName: null,
+      sectionId: null,
+      pageName: null,
+      pageId: null,
+      url: url,
+      reason: 'url was null or undefined',
+    };
+  }
 
+  wd = getParameterByName('wd', url);
 
-    // null or undefined case
-    if (url === "undefined" || url === null) {
-        return {
-            isValidUrl : false,
-            sectionGroupNames : [],
-            sectionName : null,
-            sectionId : null,
-            pageName : null,
-            pageId : null,
-            url : url,
-            reason : 'url was null or undefined'
-        };
-    }
+  if (wd === 'undefined' || wd === null || !isTarget(wd)) {
+    return {
+      isValidUrl: false,
+      sectionGroupNames: [],
+      sectionName: null,
+      sectionId: null,
+      pageName: null,
+      pageId: null,
+      url: url,
+      reason: 'there was no wd parameter or it did not contain a target',
+    };
+  }
 
-    wd = getParameterByName("wd", url);
+  targetInfo = retrieveTargetInformation(wd);
 
-    if (wd === "undefined" || wd === null || !isTarget(wd)) {
-        return {
-            isValidUrl : false,
-            sectionGroupNames : [],
-            sectionName : null,
-            sectionId : null,
-            pageName : null,
-            pageId : null,
-            url : url,
-            reason : 'there was no wd parameter or it did not contain a target'
-        };
-    }
+  // Section names can't contain any of the following characters: ~ # % & ( { } | \ : " < > ? / ^
+  try {
+    sectionDelimiterPosition = targetInfo.indexOf('|');
+    afterSectionDelimiter = sectionDelimiterPosition + 1;
+    targetInfoLength = targetInfo.length - 1;
 
-    targetInfo = retrieveTargetInformation(wd);
+    sectionGuid = targetInfo.substring(afterSectionDelimiter, afterSectionDelimiter + guidLength);
 
-    // Section names can't contain any of the following characters: ~ # % & ( { } | \ : " < > ? / ^
-    try {
-        sectionDelimiterPosition = targetInfo.indexOf("|");
-        afterSectionDelimiter = sectionDelimiterPosition + 1;
-        targetInfoLength = targetInfo.length - 1;
+    // format for section name in target string \\<sectionname>.one
+    sectionName = targetInfo.substring(2, sectionDelimiterPosition - 4).split('/');
+    sectionGroupNames = sectionName.slice(0, sectionName.length - 1);
+    sectionName = sectionName[sectionName.length - 1];
 
-        sectionGuid = targetInfo.substring(afterSectionDelimiter, afterSectionDelimiter + guidLength);
+    pageName = targetInfo.substring(
+      afterSectionDelimiter + guidLength + pageDelimiterLength,
+      targetInfoLength - guidLength - pageDelimiterLength
+    );
 
-        // format for section name in target string \\<sectionname>.one
-        sectionName = targetInfo.substring(2, sectionDelimiterPosition - 4).split('/');
-        sectionGroupNames = sectionName.slice(0, sectionName.length - 1);
-        sectionName = sectionName[sectionName.length -1];
+    pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength);
 
-        pageName = targetInfo.substring(afterSectionDelimiter + guidLength + pageDelimiterLength, targetInfoLength - guidLength - pageDelimiterLength);
-       
-        pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength);
-
-        return {
-            isValidUrl : true,
-            sectionGroupNames : sectionGroupNames,
-            sectionName : sectionName.replaceAll('\\', ''),
-            sectionId : sectionGuid,
-            pageName : pageName.replaceAll('\\', ''),
-            pageId : pageId,
-            url : url,
-            reason : 'success'
-        };
-    } catch (e) {
-        return {
-            isValidUrl : false,
-            sectionGroupNames : [],
-            sectionName : null,
-            sectionId : null,
-            pageName : null,
-            pageId : null,
-            url : url,
-            reason : 'unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )'
-        };
-    }
+    return {
+      isValidUrl: true,
+      sectionGroupNames: sectionGroupNames,
+      sectionName: sectionName.replaceAll('\\', ''),
+      sectionId: sectionGuid,
+      pageName: pageName.replaceAll('\\', ''),
+      pageId: pageId,
+      url: url,
+      reason: 'success',
+    };
+  } catch (e) {
+    return {
+      isValidUrl: false,
+      sectionGroupNames: [],
+      sectionName: null,
+      sectionId: null,
+      pageName: null,
+      pageId: null,
+      url: url,
+      reason: 'unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )',
+    };
+  }
 }
 
 module.exports = parse;
