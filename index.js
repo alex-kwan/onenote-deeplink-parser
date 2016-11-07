1522b3930ce04e36133d8a871ae9090912c2cbef
Alex Kwan <alexkwan089@gmail.com>
2016-11-07 00:07:15 -0800

    all files are now jslint verified

--- index.js
+++ index.js
@@ -1,140 +1,119 @@
 'use strict';
 
-// retrieves query parameters by name
-// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-function getParameterByName(name, url) {
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-  results = regex.exec(url);
-  if (!results) {
-    return null;
-  }
-
-  if (!results[2]) {
-    return '';
-  }
-
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
-function isTarget(queryParameter) {
-  return queryParameter.startsWith('target(') && queryParameter.endsWith(')');
-}
-
-function retrieveTargetInformation(queryParameter) {
-  // format of target query parameter   target(<targetinfo>)
-  return queryParameter.substring(7, queryParameter.length - 1);
-}
-
-//http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
-String.prototype.replaceAll = function (search, replacement) {
-  var target = this;
-  return target.split(search).join(replacement);
-};
-
-function parse(url) {
-  var pageDelimiterLength = 1,
-    guidLength = 36,
-    wd,
-    targetInfo,
-    sectionDelimiterPosition,
-    afterSectionDelimiter,
-    targetInfoLength,
-    sectionGuid,
-    sectionGroupNames,
-    sectionName,
-    pageName,
-    pageId;
-
-  // null or undefined case
-  if (url === 'undefined' || url === null) {
+function GetErrorResponse(message) {
     return {
-      isValidUrl: false,
-      sectionGroupNames: [],
-      sectionName: null,
-      sectionId: null,
-      pageName: null,
-      pageId: null,
-      reason: 'url was null or undefined',
-    };
-  }
-
-  wd = getParameterByName('wd', url);
-
-  if (wd === 'undefined' || wd === null || !isTarget(wd)) {
-    return {
-      isValidUrl: false,
-      sectionGroupNames: [],
-      sectionName: null,
-      sectionId: null,
-      pageName: null,
-      pageId: null,
-      reason: 'there was no wd parameter or it did not contain a target',
-    };
-  }
-
-  targetInfo = retrieveTargetInformation(wd);
-
-  // Section names can't contain any of the following characters: ~ # % & ( { } | \ : " < > ? / ^
-  // this suggests the first time we see a '|', we are able to determine what is section info
-  // and what is page info
-  try {
-    sectionDelimiterPosition = targetInfo.indexOf('|');
-
-    if (sectionDelimiterPosition == -1) {
-      return {
         isValidUrl: false,
         sectionGroupNames: [],
         sectionName: null,
         sectionId: null,
         pageName: null,
         pageId: null,
-        reason: 'deeplink did not contain delimiter between section and page info',
-      };
+        reason: message,
+    };
+}
+// retrieves query parameters by name
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) {
+        return null;
     }
 
-    afterSectionDelimiter = sectionDelimiterPosition + 1;
+    if (!results[2]) {
+        return '';
+    }
 
-    sectionGuid = targetInfo.substring(afterSectionDelimiter, afterSectionDelimiter + guidLength);
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
-    // format for section name in target string \\<sectionname>.one
-    sectionName = targetInfo.substring(2, sectionDelimiterPosition - 4).split('/');
+function isTarget(queryParameter) {
+    return queryParameter.startsWith('target(') && queryParameter.endsWith(')');
+}
 
-    // the section name can actually "contain" the section group names as well
-    sectionGroupNames = sectionName.slice(0, sectionName.length - 1);
+function retrieveTargetInformation(queryParameter) {
+    // format of target query parameter   target(<targetinfo>)
+    return queryParameter.substring(7, queryParameter.length - 1);
+}
 
-    // we know it will always be the last element if we split the section by '/'
-    sectionName = sectionName[sectionName.length - 1];
+//http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
 
-    targetInfoLength = targetInfo.length - 1;
+function parse(url) {
+    var pageDelimiterLength = 1,
+        guidLength = 36,
+        wd,
+        targetInfo,
+        sectionDelimiterPosition,
+        afterSectionDelimiter,
+        targetInfoLength,
+        sectionGuid,
+        sectionGroupNames,
+        sectionName,
+        pageName,
+        pageId;
 
-    pageName = targetInfo.substring(
-      afterSectionDelimiter + guidLength + pageDelimiterLength,
-      targetInfoLength - guidLength - pageDelimiterLength
-    );
+  // null or undefined case
+    if (url === 'undefined' || url === null) {
+        return new GetErrorResponse('url was null or undefined');
+    }
 
-    pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength);
+    wd = getParameterByName('wd', url);
 
-    return {
-      isValidUrl: true,
-      sectionGroupNames: sectionGroupNames,
-      sectionName: sectionName.replaceAll('\\', ''),
-      sectionId: sectionGuid,
-      pageName: pageName.replaceAll('\\', ''),
-      pageId: pageId,
-      reason: 'success',
-    };
-  } catch (e) {
-    return {
-      isValidUrl: false,
-      sectionGroupNames: [],
-      sectionName: null,
-      sectionId: null,
-      pageName: null,
-      pageId: null,
-      reason: 'unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )',
-    };
-  }
+    if (wd === 'undefined' || wd === null || !isTarget(wd)) {
+        return new GetErrorResponse('there was no wd parameter or it did not contain a target');
+    }
+
+    targetInfo = retrieveTargetInformation(wd);
+
+    // Section names can't contain any of the following characters: ~ # % & ( { } | \ : " < > ? / ^
+    // this suggests the first time we see a '|', we are able to determine what is section info
+    // and what is page info
+    try {
+        sectionDelimiterPosition = targetInfo.indexOf('|');
+
+        if (sectionDelimiterPosition === -1) {
+            return new GetErrorResponse('deeplink did not contain delimiter between section and page info');
+        }
+
+        afterSectionDelimiter = sectionDelimiterPosition + 1;
+
+        sectionGuid = targetInfo.substring(afterSectionDelimiter, afterSectionDelimiter + guidLength);
+
+        // format for section name in target string \\<sectionname>.one
+        sectionName = targetInfo.substring(2, sectionDelimiterPosition - 4).split('/');
+
+        // the section name can actually "contain" the section group names as well
+        sectionGroupNames = sectionName.slice(0, sectionName.length - 1);
+
+        // we know it will always be the last element if we split the section by '/'
+        sectionName = sectionName[sectionName.length - 1];
+
+        targetInfoLength = targetInfo.length - 1;
+
+        pageName = targetInfo.substring(
+            afterSectionDelimiter + guidLength + pageDelimiterLength,
+            targetInfoLength - guidLength - pageDelimiterLength
+        );
+
+        pageId = targetInfo.substring(targetInfoLength - guidLength, targetInfoLength);
+
+        return {
+            isValidUrl: true,
+            sectionGroupNames: sectionGroupNames,
+            sectionName: sectionName.replaceAll('\\', ''),
+            sectionId: sectionGuid,
+            pageName: pageName.replaceAll('\\', ''),
+            pageId: pageId,
+            reason: 'success',
+        };
+    } catch (e) {
+        return new GetErrorResponse('unable to parse url : expected format( <sectionname>.one|<guid>/<pagename>|<guid )');
+    }
 }
 
 module.exports = parse;
--- spec/ComplexValidParseSpec.js
+++ spec/ComplexValidParseSpec.js
@@ -1,8 +1,11 @@
+"use strict";
+
 var parser = require('../index.js');
-describe("onenote valid complex deeplinks", function(){
-     it("parsing link with brackets in section name", function() {
-        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA%5C)%5C).one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FBBB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29';
-        var result = parser(onedriveLink);
+
+describe("onenote valid complex deeplinks", function () {
+    it("parsing link with brackets in section name", function () {
+        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA%5C)%5C).one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FBBB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29',
+            result = parser(onedriveLink);
         expect(result.isValidUrl).toBe(true);
         expect(result.sectionGroupNames).toEqual([]);
         expect(result.sectionName).toBe('AAA\)\)');
@@ -12,9 +15,9 @@ describe("onenote valid complex deeplinks", function(){
         expect(result.reason).toBe('success');
     });
 
-    it("parsing link with section/page delimiter in page name", function() {
-        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2F%5C%7CBBB%5C%7C%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29';
-        var result = parser(onedriveLink);
+    it("parsing link with section/page delimiter in page name", function () {
+        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2F%5C%7CBBB%5C%7C%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29',
+            result = parser(onedriveLink);
         expect(result.isValidUrl).toBe(true);
         expect(result.sectionGroupNames).toEqual([]);
         expect(result.sectionName).toBe('AAA');
@@ -24,9 +27,9 @@ describe("onenote valid complex deeplinks", function(){
         expect(result.reason).toBe('success');
     });
 
-    it("parsing link with multiple forward slash in page name", function() {
-        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FB%5C%2FB%5C%2FB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29';
-        var result = parser(onedriveLink);
+    it("parsing link with multiple forward slash in page name", function () {
+        var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FB%5C%2FB%5C%2FB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29',
+            result = parser(onedriveLink);
         expect(result.isValidUrl).toBe(true);
         expect(result.sectionGroupNames).toEqual([]);
         expect(result.sectionName).toBe('AAA');
