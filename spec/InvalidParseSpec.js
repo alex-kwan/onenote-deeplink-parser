292b3eb8d2c6239368d3e77f026b548452176ee4
Alex Kwan <alexkwan089@gmail.com>
2016-11-06 23:32:24 -0800

    added complex unit tests

--- spec/InvalidParseSpec.js
+++ spec/InvalidParseSpec.js
@@ -1,8 +1,7 @@
 var parser = require('../index.js');
 
 describe("onenote invalid deeplinks", function(){
-    var baseUrl = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a'+
-        '&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote';
+    
 
     describe("parsing no link", function() {
         it("parsing null link", function() {
@@ -20,6 +19,7 @@ describe("onenote invalid deeplinks", function(){
 
     describe("parsing link with no query params", function(){
         it("parsing no wd param", function() {
+            var baseUrl = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote';
             var result = parser(baseUrl);
             expect(result.isValidUrl).toBe(false);
             expect(result.reason).toBe('there was no wd parameter or it did not contain a target');
--- spec/ValidParseSpec.js
+++ spec/ValidParseSpec.js
@@ -1,28 +1,35 @@
 var parser = require('../index.js');
 
 describe("onenote valid deeplinks", function(){
+    
+    var expectedSectionName = 'AAA';
+    var expectedSectionId = '111a1a11-a11a-11a1-1a1a-a111a111aa1a';
+    var expectedPageName = 'BBB'
+    var expectedPageId = '111a11aa-111a-1aaa-111a-11a1111a1a1a';
+    var expectedReason = 'success';
+
     describe("parsing link", function() {
         it("parsing onedrive link", function() {
             var onedriveLink = 'https://onedrive.live.com/edit.aspx?cid=1a1aa11aa11aa11a&id=documents&resid=1A1AA11AA11AA11A!111&app=OneNote&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FBBB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29';
             var result = parser(onedriveLink);
             expect(result.isValidUrl).toBe(true);
             expect(result.sectionGroupNames).toEqual([]);
-            expect(result.sectionName).toBe('AAA');
-            expect(result.sectionId).toBe('111a1a11-a11a-11a1-1a1a-a111a111aa1a');
-            expect(result.pageName).toBe('BBB');
-            expect(result.pageId).toBe('111a11aa-111a-1aaa-111a-11a1111a1a1a');
-            expect(result.reason).toBe('success');
+            expect(result.sectionName).toBe(expectedSectionName);
+            expect(result.sectionId).toBe(expectedSectionId);
+            expect(result.pageName).toBe(expectedPageName);
+            expect(result.pageId).toBe(expectedPageId);
+            expect(result.reason).toBe(expectedReason);
         });
         it("parsing sharepoint link", function(){
             var sharepointLink = 'https://sharepointonline.com/teams/teamawesome/_layouts/15/WopiFrame.aspx?sourcedoc={ea3d1219-b013-4a19-b301-bfdea52eb922}&action=edit&wd=target%28%2F%2FAAA.one%7C111a1a11-a11a-11a1-1a1a-a111a111aa1a%2FBBB%7C111a11aa-111a-1aaa-111a-11a1111a1a1a%2F%29';
             var result = parser(sharepointLink);
             expect(result.isValidUrl).toBe(true);
             expect(result.sectionGroupNames).toEqual([]);
-            expect(result.sectionName).toBe('AAA');
-            expect(result.sectionId).toBe('111a1a11-a11a-11a1-1a1a-a111a111aa1a');
-            expect(result.pageName).toBe('BBB');
-            expect(result.pageId).toBe('111a11aa-111a-1aaa-111a-11a1111a1a1a');
-            expect(result.reason).toBe('success');
+            expect(result.sectionName).toBe(expectedSectionName);
+            expect(result.sectionId).toBe(expectedSectionId);
+            expect(result.pageName).toBe(expectedPageName);
+            expect(result.pageId).toBe(expectedPageId);
+            expect(result.reason).toBe(expectedReason);
         });
     });
     describe("parsing links with section groups", function(){
@@ -31,11 +38,11 @@ describe("onenote valid deeplinks", function(){
             var result = parser(onedriveLink);
             expect(result.isValidUrl).toBe(true);
             expect(result.sectionGroupNames).toEqual(['B', 'BB']);
-            expect(result.sectionName).toBe('AAA');
-            expect(result.sectionId).toBe('111a1a11-a11a-11a1-1a1a-a111a111aa1a');
-            expect(result.pageName).toBe('BBB');
-            expect(result.pageId).toBe('111a11aa-111a-1aaa-111a-11a1111a1a1a');
-            expect(result.reason).toBe('success');
+            expect(result.sectionName).toBe(expectedSectionName);
+            expect(result.sectionId).toBe(expectedSectionId);
+            expect(result.pageName).toBe(expectedPageName);
+            expect(result.pageId).toBe(expectedPageId);
+            expect(result.reason).toBe(expectedReason);
         });
 
         it("parsing sharepoint link with section groups", function(){
@@ -43,11 +50,11 @@ describe("onenote valid deeplinks", function(){
             var result = parser(sharepointLink);
             expect(result.isValidUrl).toBe(true);
             expect(result.sectionGroupNames).toEqual(['AA','AAA','AAAA']);
-            expect(result.sectionName).toBe('AAA');
-            expect(result.sectionId).toBe('111a1a11-a11a-11a1-1a1a-a111a111aa1a');
-            expect(result.pageName).toBe('BBB');
-            expect(result.pageId).toBe('111a11aa-111a-1aaa-111a-11a1111a1a1a');
-            expect(result.reason).toBe('success');
+            expect(result.sectionName).toBe(expectedSectionName);
+            expect(result.sectionId).toBe(expectedSectionId);
+            expect(result.pageName).toBe(expectedPageName);
+            expect(result.pageId).toBe(expectedPageId);
+            expect(result.reason).toBe(expectedReason);
         });
     });
 });
